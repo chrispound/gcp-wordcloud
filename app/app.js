3a31e454376dc959b1e7ee9996349691acf4b423
Chris Pound <devsirchip@gmail>
2017-04-06 22:25:10 -0700

    Messaging system working

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+let message = "No messages yet.";
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+app.use(bodyParser.json());
+app.set('view engine', 'pug')
 
 const server = app.listen(8080, () => {
   const host = server.address().address;
@@ -11,3 +13,19 @@ const server = app.listen(8080, () => {
 
   console.log(`Example app listening at http://${host}:${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.render('index', {message})
+});
+
+app.post('/push', (req, res) => {
+  console.log(Object.keys(req));
+  console.log('body: ' + req.body.message);
+  console.log(message);
+  console.log('Updating message value');
+  message = `${req.body.message}`;
+  console.log(`New message value: ${req.body.message}`);
+  res.send(`Got topic push with message: ${req.body.message}`)
+});
+
+//test
